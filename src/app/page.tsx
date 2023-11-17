0ac89f9fba1af2b7d82e1d16ca9ffd61fa675d81
Yahya Eddhissa <yahya.eddhissa@proton.me>
2023-11-17 19:46:58 +0100

    Added skills and projects sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,60 @@ export const metadata: Metadata = {
     "I'm a Software Engineer currently working as the CTO and co-founder of Orderedo. I'm very enthusiastic when it comes to Developer Experience and Design Patterns, while also emphasizing on Accessibility and good User Experience. I also share what I know on my personal blog and contribute to open-source projects.",
 };
 
+function Skill() {
+  return (
+    <div>
+      <div className="mb-1.5">
+        <i className={`ti ti-world text-4xl`} />
+      </div>
+      <h4 className="text-xl font-semibold mb-2">Full Stack Web Development</h4>
+      <p className="text-sm font-normal text-gray-400">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        quidem velit consequuntur ullam tempore voluptas architecto recusandae,
+        optio nam quae.
+      </p>
+    </div>
+  );
+}
+
+function Project() {
+  return (
+    <div className="bg-sky-600 bg-opacity-10 rounded relative overflow-hidden">
+      <div className="flex items-start gap-4 px-7 py-4 mb-3">
+        <div className="flex items-center gap-4">
+          <h4 className="text-xl font-medium">Archade</h4>
+          <div className="h-0 w-10 border-t border-gray-500 flex-none" />
+        </div>
+        <p className="font-normal">
+          Archade is an open-source social network, made for architects and
+          construction engineers, plus all building enthusiasts
+        </p>
+      </div>
+      <div className="flex">
+        <div className="ml-auto w-[90%]">
+          <div className="flex gap-2 text-xs font-normal mb-3">
+            {["Social Networking", "Discussions", "Authentication"].map(
+              (item) => (
+                <div
+                  className="flex h-7 items-center px-4 bg-white bg-opacity-5 rounded-full"
+                  key={Math.random()}
+                >
+                  {item}
+                </div>
+              )
+            )}
+          </div>
+          <div className="h-60 w-full bg-sky-600 rounded-tl-md"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <main>
-      <div>
+      <div className="h-screen border-b border-b-gray-800">
         <header className="flex md:justify-between justify-center items-center h-20 md:px-24 px-6 w-screen">
           <div className="flex items-center gap-2 flex-none">
             <div className="md:h-8 md:w-8 w-6 h-6 bg-green-800 rounded-full flex-none" />
@@ -36,14 +86,14 @@ export default function Home() {
           <h1 className="md:text-5xl text-3xl font-semibold mb-6">
             Design and Code Exceptional Online Experiences.
           </h1>
-          <h3 className="text-sm text-gray-400 mb-10 md:block hidden">
+          <p className="text-sm text-gray-400 mb-10 md:block hidden">
             I&apos;m a Software Engineer currently working as the CTO and
             co-founder of <span className="underline text-white">Orderedo</span>
             . I&apos;m very enthusiastic when it comes to Developer Experience
             and Design Patterns, while also emphasizing on Accessibility and
             good User Experience. I also share what I know on my personal blog
             and contribute to open-source projects.
-          </h3>
+          </p>
           <div className="flex md:gap-6 gap-4 items-center justify-center md:justify-normal">
             <div className="flex-none">
               <Button>View Projects</Button>
@@ -57,6 +107,42 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <section className="md:px-24 px-6 py-20">
+        <div className="mb-3">
+          <h3 className="text-4xl font-semibold text-green-600 mb-1">
+            What I do best
+          </h3>
+          <div className="h-0 w-32 border-t-2 border-green-600" />
+        </div>
+        <p className="text-gray-300 mb-12">
+          I have more than 2 years of experience building web platforms and
+          services for clients all over the world. This work involves many
+          technical skillsets and knowledge summarized below.
+        </p>
+        <div className="grid grid-cols-3 gap-10">
+          <Skill />
+          <Skill />
+          <Skill />
+        </div>
+      </section>
+      <section className="md:px-24 px-6 py-20 border-y border-gray-700">
+        <div className="mb-3">
+          <h3 className="text-4xl font-semibold text-green-600 mb-1">
+            Featured Projects
+          </h3>
+          <div className="h-0 w-32 border-t-2 border-green-600" />
+        </div>
+        <p className="text-gray-300 mb-12">
+          Below is a featued list of the most remarkable projects I built over
+          the years. Some of them are open-source, some of them are not.
+        </p>
+        <div className="grid grid-cols-2 gap-5">
+          <Project />
+          <Project />
+          <Project />
+          <Project />
+        </div>
+      </section>
     </main>
   );
 }
