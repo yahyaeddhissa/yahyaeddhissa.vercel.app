714633e21c8d3b6920f1b01d4e473641d10972ca
Yahya Eddhissa <yahya.eddhissa@proton.me>
2023-11-21 19:33:46 +0100

    Made the footer responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -159,7 +159,7 @@ export default function Home() {
           </div>
         </Section>
       </main>
-      <footer className="h-16 px-24 flex items-center justify-center">
+      <footer className="h-16 px-5 text-xs md:text-sm flex items-center justify-center">
         <div>
           &#169; 2023-2024. Made With Love By
           <Link href="/" className="text-green-600 underline ml-1">
