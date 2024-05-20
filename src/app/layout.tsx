681bd408d30d4af81256642bebedf7d30b9289e4
Yahya Eddhissa <yahya.eddhissa@proton.me>
2024-05-20 14:37:02 +0000

    Add Google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta
+          name="google-site-verification"
+          content="PlqSUAjVqkvMAov71kv_Wb1oyEgrLKz60zTb4-iwsIY"
+        />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css"
